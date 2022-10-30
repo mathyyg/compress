3a8090a5e9980994e41ca777b7d7e1d9f23e15e8
Mathys <mathgyy@gmail.com>
2022-10-30 15:00:47 +0100

    Update stats template (container glitch)

--- src/Repository/UtilisationRepository.php
+++ src/Repository/UtilisationRepository.php
@@ -42,10 +42,10 @@ class UtilisationRepository extends ServiceEntityRepository
     public function findAllForResource($resource_id, $user, $admin = false) {
         $query = $this->createQueryBuilder('u');
         if(!$user) {
-            $query->select('u.date');
+            $query->select('u.date', 'u.url');
         }
         if ($admin) {
-            $query->select('u.date', 'u.ip');
+            $query->select('u.date', 'u.ip', 'u.url');
         }
         $query->andWhere('u.resource = :val1')
               ->setParameter('val1', $resource_id);
